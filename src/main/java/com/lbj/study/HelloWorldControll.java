42a3299d58b318006644325a2413137e338fc4b7
xr <1172907815@qq.com>
2018-10-30 00:42:41 +0800

    增加了注释

--- src/main/java/com/lbj/study/HelloWorldControll.java
+++ src/main/java/com/lbj/study/HelloWorldControll.java
@@ -1,5 +1,6 @@
 package com.lbj.study;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldControll {
 
+    //20181029学习第一个springboot程序
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String ShowHello(){
         return "Hello World!";
