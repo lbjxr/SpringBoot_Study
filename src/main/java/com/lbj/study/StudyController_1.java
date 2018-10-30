107efe7432e2a2ed8bae230e968cd55cfdade420
xr <1172907815@qq.com>
2018-10-30 21:39:39 +0800

    Controller注解的使用

---
+++ src/main/java/com/lbj/study/StudyController_1.java
@@ -0,0 +1,26 @@
+package com.lbj.study;
+
+/*
+*传统注解@Controller
+* 如果传json格式的数据，需要配合@ResponseBody使用
+  */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class StudyController_1 {
+
+    @Value("${age}")
+    private Integer age;
+
+    @RequestMapping(value = "/showAge", method = RequestMethod.GET)
+    public String showAge(){
+        return "age:" + age;
+    }
+
+}
